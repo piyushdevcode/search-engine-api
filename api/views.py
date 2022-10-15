d5fa40895ba25f84ba6d1708f2318159c05d122b
piyushdevcode <17671660+piyushdevcode@users.noreply.github.com>
2022-10-15 20:21:56 +0530

    drf initial setup

--- api/views.py
+++ api/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
 from django.shortcuts import HttpResponse
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
 
+@api_view()
 def home(request):
-    return HttpResponse("Home")
+    my_data = {"name": "John Doe"}
+    return Response(my_data, status=status.HTTP_200_OK)
